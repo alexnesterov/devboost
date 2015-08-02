3dffc86e01f76b32db1af7b4296d764d5045cce1
Alex Nesterov <alex.def347@gmail.com>
2015-08-03 02:45:42 +0300

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,57 +8,72 @@ var connect = require('gulp-connect');
 var open = require('gulp-open');
 var imagemin = require('gulp-imagemin');
 
+var appFolder = {
+  fonts: "app/assets/fonts",
+  images: "app/assets/images",
+  styles: "app/assets/styles",
+  scripts: "app/assets/scripts",
+  templates: "app/templates"
+}
 
-// Jade Task
-gulp.task('jade', function() {
-  gulp.src('app/jade/*.jade')
+var distFolder = {
+  fonts: "dist/assets/fonts",
+  images: "dist/assets/images",
+  styles: "dist/assets/styles",
+  scripts: "dist/assets/scripts",
+  templates: "dist"
+}
+
+// Templates Task
+gulp.task('templates', function() {
+  gulp.src(appFolder.templates + '/*.jade')
     .pipe(plumber())
     .pipe(jade({
       pretty: true
     }))
-    .pipe(gulp.dest('public'))
+    .pipe(gulp.dest(distFolder.templates))
     .pipe(connect.reload());
 });
 
-// Stylus Task
-gulp.task('stylus', function() {
-  gulp.src('app/styl/*.styl')
+// Styles Task
+gulp.task('styles', function() {
+  gulp.src(appFolder.styles + '/*.styl')
     .pipe(plumber())
     .pipe(stylus({
       'include css': true,
       use: [nib()]
     }))
     //.pipe(minifyCss())
-    .pipe(gulp.dest('public/css'))
+    .pipe(gulp.dest(distFolder.styles))
     .pipe(connect.reload());
 });
 
 //Images Task
 gulp.task('images', function() {
-  gulp.src('app/img/**/*')
+  gulp.src(appFolder.images + '/**/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('public/img'))
+    .pipe(gulp.dest(distFolder.images))
     .pipe(connect.reload());
 });
 
 //Scripts Task
-gulp.task('js', function() {
+gulp.task('scripts', function() {
   gulp.src('app/components/jquery/dist/jquery.min.js')
-    .pipe(gulp.dest('public/js/vendor'));
-  gulp.src('app/js/main.js')
-    .pipe(gulp.dest('public/js'))
+    .pipe(gulp.dest(distFolder.scripts + '/vendor'));
+  gulp.src(appFolder.scripts + '/main.js')
+    .pipe(gulp.dest(distFolder.scripts))
     .pipe(connect.reload());
 });
 
 // Watching files task
-gulp.task('watch', ['jade', 'stylus', 'js'], function() {
-  gulp.watch('app/jade/**/*.jade', ['jade']);
-  gulp.watch('app/styl/**/*.styl', ['stylus']);
-  gulp.watch('app/img/**/*', ['images']);
-  gulp.watch('app/js/**/*', ['js']);
+gulp.task('watch', ['templates', 'styles', 'scripts'], function() {
+  gulp.watch(appFolder.templates + '/**/*.jade', ['templates']);
+  gulp.watch(appFolder.styles + '/**/*.styl', ['styles']);
+  gulp.watch(appFolder.images + '/**/*', ['images']);
+  gulp.watch(appFolder.scripts + '/**/*', ['scripts']);
 });
 
-// Open task
+// Server tasks
 gulp.task('open', function() {
   gulp.src(__filename)
     .pipe(open({
@@ -70,7 +85,7 @@ gulp.task('open', function() {
 gulp.task('server', function() {
   connect.server({
     port: 8888,
-    root: 'public',
+    root: 'dist',
     livereload: true
   });
 });
