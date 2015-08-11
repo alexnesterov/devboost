378a2aafc10b13b763427a334015d1d0ccb8c6a2
Alex Nesterov <alex.def347@gmail.com>
2015-08-11 17:16:37 +0300

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -8,79 +8,84 @@ var imagemin = require('gulp-imagemin');
 var browserSync = require('browser-sync').create();
 var clean = require('gulp-clean');
 
-var appFolder = {
-  fonts: 'app/assets/fonts',
-  img: 'app/assets/img',
-  css: 'app/assets/css',
-  js: 'app/assets/js',
-  templates: 'app/templates'
+var path = {
+  app: {
+    html: 'app/*.jade',
+    css: 'app/assets/css/*.styl',
+    js: 'app/assets/js/*.js',
+    img: 'app/assets/img/**/*',
+    fonts: 'app/assets/fonts/**/*'
+  },
+  dist: {
+    html: 'dist',
+    css: 'dist/assets/css',
+    js: 'dist/assets/js',
+    img: 'dist/assets/img',
+    fonts: 'dist/assets/fonts'
+  },
+  watch: {
+    html: [
+      'app/*.jade',
+      'app/layouts/*.jade'
+    ],
+    css: 'app/assets/css/**/*',
+    js: 'app/assets/js/**/*',
+    img: 'app/assets/img/**/*',
+    fonts: 'app/assets/fonts/**/*'
+  }
 }
 
-var distFolder = {
-  fonts: 'dist/assets/fonts',
-  img: 'dist/assets/img',
-  css: 'dist/assets/css',
-  js: 'dist/assets/js',
-  templates: 'dist'
-}
-
-//Fonts Tasks
-gulp.task('fonts', function() {
-  gulp.src(appFolder.fonts + '/**/*')
-  gulp.dest(distFolder.fonts);
-});
-
-//Images Task
-gulp.task('img', function() {
-  gulp.src(appFolder.img + '/**/*')
+// Templates Task
+gulp.task('html', function() {
+  gulp.src(path.app.html)
     .pipe(plumber())
-    .pipe(imagemin())
-    .pipe(gulp.dest(distFolder.img))
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest(path.dist.html))
     .pipe(browserSync.stream());
 });
 
 // Styles Task
 gulp.task('css', function() {
-  gulp.src(appFolder.css + '/*.styl')
+  gulp.src(path.app.css)
     .pipe(plumber())
     .pipe(stylus({
       'include css': true,
       use: [nib()]
     }))
     //.pipe(minifyCss())
-    .pipe(gulp.dest(distFolder.css))
+    .pipe(gulp.dest(path.dist.css))
     .pipe(browserSync.stream());
 });
 
 //Scripts Task
 gulp.task('js', function() {
-  gulp.src('app/components/jquery/dist/jquery.min.js')
-    .pipe(gulp.dest(distFolder.js + '/vendor'))
+  gulp.src('bower_components/jquery/dist/jquery.min.js')
+    .pipe(gulp.dest(path.dist.js + '/vendor'))
     .pipe(browserSync.stream());
-  gulp.src(appFolder.js + '/common.js')
-    .pipe(gulp.dest(distFolder.js))
+  gulp.src(path.app.js)
+    .pipe(gulp.dest(path.dist.js))
     .pipe(browserSync.stream());
 });
 
-// Templates Task
-gulp.task('templates', function() {
-  gulp.src(appFolder.templates + '/*.jade')
+//Images Task
+gulp.task('img', function() {
+  gulp.src(path.app.img)
     .pipe(plumber())
-    .pipe(jade({
-      pretty: true
-    }))
-    .pipe(gulp.dest(distFolder.templates))
+    .pipe(imagemin())
+    .pipe(gulp.dest(path.dist.img))
     .pipe(browserSync.stream());
 });
 
-// Watching files task
-gulp.task('watch', ['fonts', 'img', 'css', 'js', 'templates'], function() {
-  gulp.watch(appFolder.fonts + '/**/*', ['fonts']);
-  gulp.watch(appFolder.img + '/**/*', ['img']);
-  gulp.watch(appFolder.css + '/**/*.styl', ['css']);
-  gulp.watch(appFolder.js + '/**/*', ['js']);
-  gulp.watch(appFolder.templates + '/**/*.jade', ['templates']);
+//Fonts Tasks
+gulp.task('fonts', function() {
+  gulp.src(path.app.fonts)
+    .pipe(gulp.dest(path.dist.fonts));
 });
+
+// Build project
+gulp.task('build', ['html', 'css', 'js', 'img', 'fonts']);
 
 // Browsersync Task
 gulp.task('server', function() {
@@ -92,6 +97,15 @@ gulp.task('server', function() {
   });
 });
 
+// Watching files task
+gulp.task('watch', function() {
+  gulp.watch(path.watch.html, ['html']);
+  gulp.watch(path.watch.css, ['css']);
+  gulp.watch(path.watch.js, ['js']);
+  gulp.watch(path.watch.img, ['img']);
+  gulp.watch(path.watch.fonts, ['fonts']);
+});
+
 // Clean Task
 gulp.task('clean', function() {
   return gulp.src('dist', {read: false})
@@ -100,37 +114,4 @@ gulp.task('clean', function() {
 
 // Default task
 // Run development environment
-gulp.task('default', ['watch', 'server']);
-
-// Build project
-gulp.task('build', function() {
-  // fonts
-  gulp.src(appFolder.fonts + '/**/*')
-  gulp.dest(distFolder.fonts);
-  // img
-  gulp.src(appFolder.img + '/**/*')
-    .pipe(plumber())
-    .pipe(imagemin())
-    .pipe(gulp.dest(distFolder.img));
-  // css
-  gulp.src(appFolder.css + '/*.styl')
-    .pipe(plumber())
-    .pipe(stylus({
-      'include css': true,
-      use: [nib()]
-    }))
-    .pipe(minifyCss())
-    .pipe(gulp.dest(distFolder.css));
-  // js
-  gulp.src('app/components/jquery/dist/jquery.min.js')
-    .pipe(gulp.dest(distFolder.js + '/vendor'));
-  gulp.src(appFolder.js + '/common.js')
-    .pipe(gulp.dest(distFolder.js));
-  // templates
-  gulp.src(appFolder.templates + '/*.jade')
-    .pipe(plumber())
-    .pipe(jade({
-      pretty: true
-    }))
-    .pipe(gulp.dest(distFolder.templates));
-});
+gulp.task('default', ['build', 'server', 'watch']);
