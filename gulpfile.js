af05888ac768c2572c8b6923d496e86d052e25db
Alex Nesterov <ialexnesterov@gmail.com>
2018-04-24 19:17:42 +0300

    Add path vars in gulpfile and json data for pug

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
 var cssnano = require('gulp-cssnano');
+var fs = require('fs');
+var data = require('gulp-data');
 var pug = require('gulp-pug');
 var plumber = require('gulp-plumber');
 var notify = require('gulp-notify');
@@ -14,8 +16,52 @@ var reload = browserSync.reload;
 var zip = require('gulp-zip');
 var ghPages = require('gulp-gh-pages');
 
+var PROJECT_NAME = 'devboost';
+var PATH = {
+  app: {
+    html: ['app/*.pug', '!app/config.pug'],
+    styles: 'app/assets/styles/*.scss',
+    scripts: 'app/assets/scripts/*.js',
+    images: 'app/assets/images/**/*',
+    fonts: 'app/assets/fonts/**/*',
+    rootfiles: ['app/*.*', '!app/*.pug']
+  },
+  dist: {
+    html: 'dist',
+    styles: 'dist/assets/styles/',
+    scripts: 'dist/assets/scripts/',
+    images: 'dist/assets/images/',
+    fonts: 'dist/assets/fonts/',
+    rootfiles: 'dist'
+  },
+  watch: {
+    html: 'dist/*.html',
+    pug: 'app/**/*.pug',
+    styles: 'app/assets/styles/**/*',
+    scripts: 'app/assets/scripts/**/*',
+    images: 'app/assets/images/**/*',
+    fonts: 'app/assets/fonts/**/*',
+    rootfiles: ['app/*.*', '!app/*.pug']
+  },
+  clean: './dist',
+  serve: 'dist',
+  zip: 'dist/**/*',
+  data: 'app/_data/data.json'
+};
+
+gulp.task('html', function() {
+  return gulp.src(PATH.app.html)
+    .pipe(data(function(file) {
+      return JSON.parse(fs.readFileSync(PATH.data));
+    }))
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest(PATH.dist.html));
+});
+
 gulp.task('styles', function() {
-  return gulp.src('app/assets/styles/*.scss')
+  return gulp.src(PATH.app.styles)
     .pipe(plumber({
       errorHandler: notify.onError(function(err) {
         return {
@@ -33,38 +79,30 @@ gulp.task('styles', function() {
     .pipe(cssnano({
       autoprefixer: false
     }))
-    .pipe(gulp.dest('dist/assets/styles/'))
+    .pipe(gulp.dest(PATH.dist.styles))
     .pipe(reload({stream: true}));
 });
 
 gulp.task('scripts', function() {
-  return gulp.src('app/assets/scripts/*.js')
-    .pipe(gulp.dest('dist/assets/scripts/'))
+  return gulp.src(PATH.app.scripts)
+    .pipe(gulp.dest(PATH.dist.scripts))
     .pipe(reload({stream: true}));
 });
 
-gulp.task('html', function() {
-  return gulp.src(['app/*.pug', '!app/config.pug'])
-    .pipe(pug({
-      pretty: true
-    }))
-    .pipe(gulp.dest('dist'));
-});
-
 gulp.task('images', function() {
-  return gulp.src('app/assets/images/**/*')
+  return gulp.src(PATH.app.images)
     .pipe(cache(imagemin()))
-    .pipe(gulp.dest('dist/assets/images/'));
+    .pipe(gulp.dest(PATH.dist.images));
 });
 
 gulp.task('fonts', function() {
-  return gulp.src('app/assets/fonts/**/*')
-    .pipe(gulp.dest('dist/assets/fonts/'));
+  return gulp.src(PATH.app.fonts)
+    .pipe(gulp.dest(PATH.dist.fonts));
 });
 
 gulp.task('rootfiles', function() {
-  return gulp.src(['app/*.*', '!app/*.pug'])
-    .pipe(gulp.dest('dist'));
+  return gulp.src(PATH.app.rootfiles)
+    .pipe(gulp.dest(PATH.dist.rootfiles));
 });
 
 gulp.task('clear', function(done) {
@@ -72,7 +110,7 @@ gulp.task('clear', function(done) {
 });
 
 gulp.task('clean', ['clear'], function() {
-  return del('dist');
+  return del(PATH.clean);
 });
 
 gulp.task('build', ['clean'], function(callback) {
@@ -88,29 +126,29 @@ gulp.task('build', ['clean'], function(callback) {
 gulp.task('serve', ['build'], function() {
   browserSync.init({
     server: {
-      baseDir: 'dist',
+      baseDir: PATH.serve,
     },
     port: 1508,
     notify: false
   });
 
-  gulp.watch('dist/*.html').on('change', reload);
-  gulp.watch('app/**/*.pug', ['html']);
-  gulp.watch(['app/*.*', '!app/*.pug'], ['rootfiles']);
-  gulp.watch('app/assets/styles/**/*', ['styles']);
-  gulp.watch('app/assets/scripts/**/*', ['scripts']);
-  gulp.watch('app/assets/images/**/*', ['images']);
-  gulp.watch('app/assets/fonts/**/*', ['fonts']);
+  gulp.watch(PATH.watch.html).on('change', reload);
+  gulp.watch(PATH.watch.pug, ['html']);
+  gulp.watch(PATH.watch.styles, ['styles']);
+  gulp.watch(PATH.watch.scripts, ['scripts']);
+  gulp.watch(PATH.watch.images, ['images']);
+  gulp.watch(PATH.watch.fonts, ['fonts']);
+  gulp.watch(PATH.watch.rootfiles, ['rootfiles']);
 });
 
 gulp.task('zip', function() {
-  return gulp.src('dist/**/*')
-    .pipe(zip('devboost.pack.zip'))
+  return gulp.src(PATH.zip)
+    .pipe(zip(PROJECT_NAME + '.pack.zip'))
     .pipe(gulp.dest('./'));
 });
 
 gulp.task('gh-pages', ['build'], function() {
-  return gulp.src('./dist/**/*')
+  return gulp.src(PATH.serve + '/**/*')
     .pipe(ghPages());
 });
 
